4822baa24956c79e120851e531c2373d1ee3427a
anaypant <35899820+anaypant@users.noreply.github.com>
2021-09-03 14:25:33 -0700

    c

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class MyMain {
     // Adds 5 to the given integer
     // This method has been written for you
@@ -9,34 +11,39 @@ public class MyMain {
     // returns the largest of the three numbers
     public static int findBiggestNumber(int x, int y, int z) {
         // REPLACE THIS WITH YOUR CODE
-        return 0;
+        return Math.max(Math.max(x, y), z);
     }
 
     // This method simulates the flip of a coin, where
     // true represents "heads" and false represents "tails"
     // This method should return true half the time, and false the other half
-    public static boolean flipCoin() {
-        // REPLACE THIS WITH YOUR CODE
-        return false;
+    public static boolean flipCoin(double coin_chance) {
+        return (Math.random() >= coin_chance);
     }
 
-    // This method simulates the flip of a weighted coin,
-    // that is a coin that is biased towards heads
+    public static boolean flipCoin() {
+        double coin = 0.5;
+        return flipCoin(coin);
+    }
+
+    //     This method simulates the flip of a weighted coin,
+//     that is a coin that is biased towards heads
     public static boolean flipWeightedCoin() {
-        // REPLACE THIS WITH YOUR CODE
-        return false;
+        double coin = 0.2;
+        return flipCoin(coin);
     }
 
     // This method rounds a double to the nearest whole number
     public static int round(double d) {
         // REPLACE THIS WITH YOUR CODE
-        return 0;
+        return (int) Math.round(d);
     }
 
     // This method returns a random number between 1 and 6, inclusive
     public static int rollDie() {
-        // REPLACE THIS WITH YOUR CODE
-        return 0;
+        Random r = new Random();
+        return r.nextInt((6 - 1) + 1) + 1;
+
     }
 
     // This method carries out the quadratic formula and *prints* out the answers
@@ -49,7 +56,12 @@ public class MyMain {
         int x = 2;
         int y = add5(x);
         System.out.println(y);
+        int z = 19;
+        int biggest_num = findBiggestNumber(x, y, z);
+        System.out.println(biggest_num);
+        System.out.println(flipCoin());
+        System.out.println(flipWeightedCoin());
+        System.out.println(rollDie());
 
-        // YOUR CODE HERE
     }
 }
